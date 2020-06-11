099a140ab6db74bb6241deb7a20f56b5afe91a71
Francis Bulus <notchera@gmail.com>
2020-06-11 07:59:55 +0100

    made formatting changes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,33 +1,33 @@
 export default {
-    preload: [
-      "https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald&family=Roboto&family=Source+Sans+Pro&display=swap",
-      "https://fonts.googleapis.com/css?family=Merriweather&display=swap",
-    ],
-    colors: {
-      darkNavyStart: "#1C3451",
-      darkNavyEnd: "#0C1F2C",
-      lightYellow: "#FCE257",
-      yellow: "#FCDD3B",
-      bookYellow: "#E5E5E5",
-    },
-    borderRadius: "3px",
-    fonts: {
-      FuturaPT:
-        "Futura PT, Montserrat, Roboto, -apple-system, system-ui, BlinkMacSystemFont",
-      FuturaPTLight:
-        "Futura PT Light, Montserrat, Roboto, -apple-system, system-ui, BlinkMacSystemFont",
-      Inconsolata: "Roboto Mono, Inconsolata, Monaco, monospace",
-      LaBelle: "La Belle Aurore, cursive",
-    },
-  
-    fontSizes: {
-      xxs: "12px",
-      xs: "14px",
-      sm: "15.5px",
-      md: "18px",
-      l: "20px",
-      xl: "22px",
-      xxl: "25px",
-      ttl: "34px",
-    },
-  }
+  preload: [
+    "https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald&family=Roboto&family=Source+Sans+Pro&display=swap",
+    "https://fonts.googleapis.com/css?family=Merriweather&display=swap",
+  ],
+  colors: {
+    darkNavyStart: "#1C3451",
+    darkNavyEnd: "#0C1F2C",
+    lightYellow: "#FCE257",
+    yellow: "#FCDD3B",
+    bookYellow: "#E5E5E5",
+  },
+  borderRadius: "3px",
+  fonts: {
+    FuturaPT:
+      "Futura PT, Montserrat, Roboto, -apple-system, system-ui, BlinkMacSystemFont",
+    FuturaPTLight:
+      "Futura PT Light, Montserrat, Roboto, -apple-system, system-ui, BlinkMacSystemFont",
+    Inconsolata: "Roboto Mono, Inconsolata, Monaco, monospace",
+    LaBelle: "La Belle Aurore, cursive",
+  },
+
+  fontSizes: {
+    xxs: "12px",
+    xs: "14px",
+    sm: "15.5px",
+    md: "18px",
+    l: "20px",
+    xl: "22px",
+    xxl: "25px",
+    ttl: "34px",
+  },
+};
--- src/utils/convertStyle.js
+++ src/utils/convertStyle.js
@@ -1,29 +1,30 @@
 export function containsRvh(propertyValue) {
-    const rvhRegex = /(\d+(\.\d*)?)rvh(?!\w)/
-    return rvhRegex.test(propertyValue);
-  }
-  
-      function replaceRvhWithPx(propertyStringValue, windowHeight) {
-    const rvhRegex = /(\d+(\.\d*)?)rvh(?!\w)/g
-    return propertyStringValue.replace(
-      rvhRegex,
-      (_, rvh) => `${(windowHeight * parseFloat(rvh)) / 100}px`
-    )
-  }
-  
+  const rvhRegex = /(\d+(\.\d*)?)rvh(?!\w)/;
+  return rvhRegex.test(propertyValue);
+}
 
-  function convertStyle(windowHeight) {
-    const defaultStyle = { height: "100rvh" }
-    const usedStyle =  defaultStyle
-  
-    const convertedStyle = {}
-    Object.keys(usedStyle).forEach(key => {
-      convertedStyle[key] =
-        typeof usedStyle[key] === "string"
-          ? replaceRvhWithPx(usedStyle[key], windowHeight)
-          : usedStyle[key]
-    })
-    return convertedStyle
-  }
-  
-  export default convertStyle
+function replaceRvhWithPx(propertyStringValue, windowHeight) {
+  const rvhRegex = /(\d+(\.\d*)?)rvh(?!\w)/g;
+  return propertyStringValue.replace(
+    rvhRegex,
+    (_, rvh) => `${(windowHeight * parseFloat(rvh)) / 100}px`
+  );
+}
+
+//ddd
+
+function convertStyle(windowHeight) {
+  const defaultStyle = { height: "100rvh" };
+  const usedStyle = defaultStyle;
+
+  const convertedStyle = {};
+  Object.keys(usedStyle).forEach((key) => {
+    convertedStyle[key] =
+      typeof usedStyle[key] === "string"
+        ? replaceRvhWithPx(usedStyle[key], windowHeight)
+        : usedStyle[key];
+  });
+  return convertedStyle;
+}
+
+export default convertStyle;
