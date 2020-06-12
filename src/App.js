911d7a943f64fa4b41cd967f2fecf4b752beb44a
Francis Bulus <notchera@gmail.com>
2020-06-12 17:49:22 +0100

    imported and added theme and global style to app file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 import { ThemeProvider } from "styled-components";
-
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
 
 const App = () => {
-    return (
-        <div>
-            Buttonmash
-        </div>
-    )
-}
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div>Buttonmash</div>
+    </ThemeProvider>
+  );
+};
 
-export default App
+export default App;
