ffa173d027ce16c666c662e5e7522f20742726d9
Francis <notchera@gmail.com>
2020-06-19 12:42:04 +0100

    building out styling for right side of nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import theme from "../styles/theme";
 
 const StyledNav = styled.nav`
   max-width: 900px;
   margin: 0 auto;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  font: ${theme.fonts.Merriweather};
 `;
 
 const RightSide = styled.div``;
 const LeftSide = styled.div``;
+const RightSideItem = styled.a`
+  text-decoration: note;
+`;
 
 const NavBar = () => {
   return (
     <StyledNav>
       <RightSide>
-
+        <RightSideItem></RightSideItem>
+        <RightSideItem></RightSideItem>
+        <RightSideItem></RightSideItem>
       </RightSide>
-      <LeftSide>
-          
-      </LeftSide>
+      <LeftSide></LeftSide>
     </StyledNav>
   );
 };
