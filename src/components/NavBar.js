5f4d4e6f578a3e0e98c3a726e7077ddfcc86b90b
Francis <notchera@gmail.com>
2020-06-19 12:30:49 +0100

    building out left and right sides of nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledNav = styled.nav`
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+`;
+
+const RightSide = styled.div``
+
+const LeftSide = styled.div``
+
+
 
 const NavBar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <StyledNav>
+
+  </StyledNav>;
 };
 
 export default NavBar;
