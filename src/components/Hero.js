e63d0c2714a0edb14243e42ff999cc0eef3b7e06
Francis <notchera@gmail.com>
2020-07-05 19:12:03 +0100

    _

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,22 +3,36 @@ import styled from "styled-components";
 import theme from "../styles/theme";
 import rdr from "../assets/images/rdr.png";
 
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const StyledImage = styled.div`
-width: 100%;
-display: flex;
-justify-content: center;
-img {
-  width: 20%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
   height: auto;
-  align-item:center;
-}
+  margin-top: 5rem;
+  img {
+    width: 480px;
+    height: auto;
+    align-item: center;
+  }
 `;
 
 const Hero = () => {
   return (
+    <>
     <StyledImage>
       <img src={rdr} alt="rdr" />
     </StyledImage>
+    <Description>
+      <p>Log games you've played.</p>
+      <p>Log games you've played.</p>
+      <p>Log games you've played.</p>
+    </Description>
+    </>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
+import styled from "styled-components";
+import theme from "../styles/theme";
+import media from "../styles/media";
+import { generateSpace } from "../styles/shared-styles";
+
+export const Section = styled.section`
+  height: 100vh;
+  max-width: 1200px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+`;
 
 const Home = () => {
   return (
-    <>
+    <Section>
       <NavBar />
       <Hero />
-    </>
+    </Section>
   );
 };
 
